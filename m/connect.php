ef40d3b611da13a0c88f3c3de03c975a143ed458
Ednan Fajri <muhammad.ednan.fg@gmail.com>
2015-06-11 10:09:54 +0700

    Revert "Upload"
    This reverts commit 39ad7e681c6f49548d16c480a2524e2e7029f759.

--- m/connect.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    $host       =   'localhost';
-    $user       =   'root';
-    $passwd     =   '';
-    $dbname     =   'plinplan_mobile';
-    
-    $connect    =   new PDO("mysql:host=$host;dbname=$dbname",$user,$passwd);
-?>
