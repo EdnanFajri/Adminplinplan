bbbca4bada26d399716bfb495ffe1256df12d34a
Ednan Fajri <muhammad.ednan.fg@gmail.com>
2015-06-11 11:49:41 +0700

    Revert "balikin"
    This reverts commit 4982da6701f48dcdb20aa31ffc997c80e4c12d3c.

--- login.php
+++ login.php
@@ -4,18 +4,15 @@
         Login
     </title>
     <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
-    <div class="container" id="contain">
+    <div class="container">
         <form class="form-signin" action="c/cekstatus.php" method="post">
             <h2 class="form-signin-heading">Introduce Yourself Sir</h2>
             <label for="inputCode" class="sr-only">Email address</label>
             <input type="text" id="inputCode" class="form-control" placeholder="Username" name="username" required>
-            <br>
             <label for="inputPassword" class="sr-only">Password</label>
             <input type="password" id="inputPassword" class="form-control" placeholder="Password" name="passwd" required>
-            <br>
             <button class="btn btn-lg btn-primary btn-block" type="submit" name="submit">Sign in</button>
         </form>
     </div>
